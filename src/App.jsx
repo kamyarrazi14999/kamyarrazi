266cea3039bb5a216f3f4059551aa731dcdab76d
kamyarrazi14999 <kami6535@gmail.com>
2025-11-15 12:18:38 +0330

    Refactor App component: remove unused state and components; integrate fetch call for posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
+
 import { useState } from "react";
 import "./App.css";
-import { Profile } from "./components/Profile";
-import { LoginForm } from "./components/LoginForm";
 
-function App() {
-  const [isLogin, setIsLogin] = useState(false);
+const App = () => {
+ const [Test] = useState(false);
 
-  // const handelState = () => {
-  //   setIsLogin((prev) => !prev);
-  // };
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((response) => response.json())
+  .then((jsonn) => console.log(json));
+
 
   return (
     <>
-    <button onClick={()=>setIsLogin(prev=> !prev)}>{isLogin ? 'خروج کاربر' : 'کلیک برای لاگین'}</button>
-      {/* {isLogin == true ? <h1 onClick={handelState}>لاگین هست</h1> : <h1 onClick={handelState}>لاگین نیستت</h1>} */}
-      {/* {isLogin == true && <h1 onClick={handelState}>لاگین هست</h1>} */}
-      {/* {isLogin == false && <h1 onClick={handelState}>لاگین نیستتت</h1>} */}
-      {isLogin ? <Profile /> : <LoginForm />}
+      
     </>
   );
 }
